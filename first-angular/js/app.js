f8aaee5b3230fe58afaecc36db39eba982311add
Peng81828 <surveysandbeermoney@gmail.com>
2015-11-09 23:40:24 -0400

    angular

---
+++ first-angular/js/app.js
@@ -0,0 +1,23 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function ($routeProvider) {
+	$routeProvider
+	.when('/', {
+		templateUrl: '../pages/myPage.html',
+		controller: 'myController'
+	})
+	.when('/second', {
+		templateUrl: '../pages/secondpage.html',
+		controller: 'secondController'
+	})
+
+});
+
+myApp.controller('secondController', function($scope) {
+		$scope.secondname = "Pingadulce";
+});
+
+myApp.controller('myController', function($scope) {
+	$scope.name = "Jose";
+	$scope.edad = "16";
+});
